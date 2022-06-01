14b8c444df9c3f4536a37aefe340949703c10761
Nikitok41 <nik777pups@gmail.com>
2022-06-02 00:34:46 +0300

    Решение задачек 41 43

--- Zadacha41/Program.cs
+++ Zadacha41/Program.cs
@@ -4,4 +4,33 @@
 -1, -7, 567, 89, 223-> 3
 */
 
+Console.Clear();
 
+Console.Write($"Введите число М (кол-во чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+
+void GetNumbers(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
+int PositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+GetNumbers(m);
+
+Console.WriteLine($"Введено чисел больше 0: {PositiveNumbers(array)} ");
